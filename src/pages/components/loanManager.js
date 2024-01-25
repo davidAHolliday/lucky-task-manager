d05dc242d8293d88b0e62846e5648100b7442f12
David Anthony Perez <107569920+dperezskillstorm@users.noreply.github.com>
2024-01-24 18:23:23 -0600

    added cards and quick payment buttons

--- src/pages/components/loanManager.js
+++ src/pages/components/loanManager.js
@@ -1,9 +1,13 @@
 import { Card, Grid, Typography } from '@mui/material';
 import axios from 'axios';
 import React, { useEffect, useState } from 'react';
+import { formatDate } from '../../utils/helperFunctions';
+import { SecurityUpdate } from '@mui/icons-material';
 
 export const LoanManager = () => {
     const [data, setData] = useState([]);
+    const [toast,setToast] = useState({display:false, message:""})
+    const [update,setUpdate] = useState(false)
 
     const fetchActiveLoans = () => {
         const url = "http://localhost:8080/banking/v1/loans";
@@ -15,10 +19,62 @@ export const LoanManager = () => {
             });
     }
 
+
+    const handleQuickPayment = (id,paymentAmount) => {
+        const url = "http://localhost:8080/banking/v1/transactions";
+        return axios.post(url,{
+            loanId: id,
+            type:"Payment",
+            amount: paymentAmount,
+            note:"Quick Payment"
+
+        })
+            .then(response => {
+                console.log(response.data)
+                setToast({display:true,message: "Successfully Made a Payment"})
+                setTimeout(()=>{
+                    setToast({display:false,message: ""})
+                    setUpdate(prev=>!prev)
+                
+
+                },3000)
+
+            })
+            .catch(error => {
+                console.error(error);
+                return [];
+            });
+    }
+
+    const handleQuickInterest = (id,paymentAmount) => {
+        const url = "http://localhost:8080/banking/v1/transactions";
+        return axios.post(url,{
+            loanId: id,
+            type:"Interest",
+            amount: paymentAmount,
+            note:"Quick Interest"
+
+        })
+            .then(response =>{ 
+                console.log(response.data)
+                setToast({display:true,message: "Successfully Made a Interest Only"})
+                setTimeout(()=>{
+                    setToast({display:false,message: ""})
+                    setUpdate(prev=>!prev)
+
+                },3000)
+            })
+            .catch(error => {
+                console.error(error);
+                return [];
+            });
+    }
+
     const fetchLoans = (loanId) => {
         const url = `http://localhost:8080/banking/v1/summary/${loanId}`;
         const response = axios.get(url)
-            .then(response => response.data)
+            .then(response =>
+                response.data)
             .catch(error => {
                 console.error(error);
                 return null;
@@ -39,14 +95,14 @@ export const LoanManager = () => {
                     }
                 }));
 
-                setData(displayList);
-            } catch (error) {
+                setData(displayList)
+                } catch (error) {
                 console.error(error);
             }
         }
 
         fetchData();
-    }, []);
+    }, [update]);
 
     console.log(data);
 
@@ -80,13 +136,27 @@ export const LoanManager = () => {
 
       return (
         <>
-            {data.map((record) => (
-                <Card style={{ padding: 10 }} key={record.loanId}>
+{toast.display && (
+  <div className="toast">
+    <h1>{toast.message}</h1>
+  </div>
+)}
+  <Card style={{ padding: 10, marginTop:"10px"}}>
+       
+    
+                </Card>
+{data.sort((a,b)=> a.summary.balance - b.summary.balance)
+  .map((record, index) => (                <Card style={{ padding: 10, marginTop:"10px" ,backgroundColor:index %2 ==0? "#ADD8E6":""
+         }} key={record.loanId}>
+                    <div style={{width:"700px",display:"flex",flexDirection:"row"}} className='record'> 
+                    <div  style={{display:"flex",flexDirection:"column"}}className='record-head'>
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
                             <div style={{ display: 'flex', flexDirection: 'column' }}>
-                                <Typography variant="button" style={{ marginBottom: 8 }}>Today's Money</Typography>
-                                <Typography variant="h5">${record.originalAmount}</Typography>
+                                <Typography variant="button" style={{ marginBottom: 8 }}>Balance</Typography>
+                                <Typography variant="h5">${record.summary.balance}</Typography>
+                                <Typography variant="button" style={{ marginTop: 16 }}>Payments Left:{record.summary.paymentsleft}</Typography>
+
                             </div>
                         </Grid>
                         <Grid item xs={4}>
@@ -97,9 +167,41 @@ export const LoanManager = () => {
                             </div>
                         </Grid>
                     </Grid>
-                    <Typography variant="button" style={{ marginTop: 16 }}>+55%</Typography>
-                    <Typography variant="body2" style={{ color: 'green' }}>since yesterday</Typography>
+                    </div>
+
+                            <div style={{  width:"90%" }}>
+                            <h1 style={{textAlign:"center", color:"green"}} >${record.summary.paymentAmount} </h1>
+                                <h1 style={{textAlign:"center"}} >{record.summary.client.firstName} {record.summary.client.lastName}</h1>
+                                <p style={{textAlign:"center"}}>{record.summary.loan.loanId}</p>
+                                <p style={{ textAlign: "center", color: 'blue' }}>
+  last update: 
+  
+  {record.summary.loan.transactions.length > 0 && (
+  <>
+    {formatDate(record.summary.loan.transactions.slice(-1)[0].createDate)} $
+    {record.summary.loan.transactions.slice(-1)[0].amount}{' '}
+    {record.summary.loan.transactions.slice(-1)[0].note}
+  </>
+)}
+</p>
+               
+
+                            </div>
+                            <div style={{ display:"grid", width:"10%",alignContent:"space-between" }}>
+                              <button onClick={()=>handleQuickPayment(record.summary.loan.loanId,record.summary.paymentAmount)} style={{justifySelf:"start",height:"40px"}}>
+                                Quick Payment</button>
+                                <button onClick={()=>handleQuickInterest(record.summary.loan.loanId,record.summary.loan.interestRate * 100)} style={{justifySelf:"end",height:"40px"}}>
+                                Interest Only</button>
+
+
+               
+
+                            </div>
+                  
+                    </div>
+
                 </Card>
+
             ))}
         </>
     );
